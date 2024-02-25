97380e303aa07a798ad9cbd69f6c3493b4018b88
Drikkx <88060062+Drikkx@users.noreply.github.com>
2024-02-25 23:19:18 +0100

    add deploy core

---
+++ deploy/core.ts
@@ -0,0 +1,25 @@
+import hre from "hardhat";
+import '@nomiclabs/hardhat-ethers'
+import '@nomiclabs/hardhat-waffle'
+import '@nomiclabs/hardhat-etherscan'
+ 
+async function main() {
+
+    console.log('Deploy UniswapV3Factory')
+    // We get the contract TestnetERC20 to deploy
+    const UniswapV3Factory = await hre.ethers.getContractFactory("UniswapV3Pool");
+    const Factory = await UniswapV3Factory.deploy({
+        gasLimit: 100000000,
+        gasPrice: 100_000
+    });
+
+    await Factory.deployed();
+
+    console.log("UniswapV3Factory deployed to:", Factory.address);
+
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
